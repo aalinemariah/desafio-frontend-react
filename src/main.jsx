930ab12228ec8db588472aa7e5b11999772278af
Aline Mariah <aline.mariaah@gmail.com>
2025-09-29 13:47:25 -0300

    fix: ajuste no main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/global.css'
-// import { Product } from './pages/Product'
+import {ProductCard} from './components/ProductCard'
+//import { Product } from './pages/Product'
+
 import { Home } from './pages/Home'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
@@ -9,7 +11,8 @@ import { Footer } from './components/Footer'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
-    {/* {* //<Product /> *} */}
+    <ProductCard />
+     {/* // {/product} */}
     <Home />
     <Footer />
   </StrictMode>,
